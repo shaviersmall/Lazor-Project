23a7248c08f8ea52966b5324879e4aa6a9f9b895
GabrielePierre <155856465+GabrielePierre@users.noreply.github.com>
2024-11-06 20:37:42 -0500

    Update new.py

--- new.py
+++ new.py
@@ -25,10 +25,12 @@ def read_BFF(file_name):
       # Laser has origin coordinates and directions
       x, y, dx, dy = map(int, line.split()[1:])
       lasers.append((x, y, dx, dy))
+      L_count += 1
     elif line.startswith('P '):
       # Points have coordinates
       x, y = map(int, line.split()[1:])
             targets.append((x, y))
+      P_count += 1
     elif line.startswith("A ") or line.startswith("B ") or line.startswith("C "):
       # Amount of blocks is equal to the number in the file.
       block_type, count = line.split()
